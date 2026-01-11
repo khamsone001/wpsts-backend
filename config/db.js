d7736946d5276d44e81c7aef4733303abb00c3b1
khamsone001 <khamsone2200000@gmail.com>
2026-01-11 19:08:23 +0700

    Fix: Remove process.exit to prevent Vercel crash on DB error

--- config/db.js
+++ config/db.js
@@ -5,9 +5,9 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB Connected...');
     } catch (err) {
-        console.error(err.message);
-        // Exit process with failure
-        process.exit(1);
+        console.error('MongoDB Connection Error:', err.message);
+        // Do not exit process in serverless environment
+        throw err;
     }
 };
 
