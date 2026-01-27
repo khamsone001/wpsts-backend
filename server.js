4372aeb87dbb3398b64c597072ec4bd31e3ff029
khamsone001 <khamsone2200000@gmail.com>
2026-01-27 11:19:26 +0700

    Refine CORS for mobile production client

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const connectDB = require('./config/db');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Simple Route for testing with DB Status
